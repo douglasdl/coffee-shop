24165281ecef1991d1df11ebfca0c08e69b3f80c
douglasdl <douglas_san@hotmail.com>
2024-10-27 04:32:12 -0300

    refactor: combine variables and functions

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,53 +1,36 @@
-let coffeCount = 0;
-let teaCount = 0;
-let milkCount = 0;
-let cokeCount = 0;
-let beerCount = 0;
+const itemCounts = {
+  coffee: 0,
+  tea: 0,
+  milk: 0,
+  coke: 0,
+  beer: 0
+}
 
 let itemsOrdered = 0;
 let totalPrice = 0;
 
-function askCoffee() {
-  coffeCount++;
-  itemsOrdered++;
-  totalPrice += 480;
-  document.getElementById("coffee-count").innerText = coffeCount;
+const prices = {
+  coffee: 480,
+  tea: 280,
+  milk: 180,
+  coke: 190,
+  beer: 580
+}
+
+function updateUI() {
+  document.getElementById("coffee-count").innerText = itemCounts.coffee;
+  document.getElementById("tea-count").innerText = itemCounts.tea;
+  document.getElementById("milk-count").innerText = itemCounts.milk;
+  document.getElementById("coke-count").innerText = itemCounts.coke;
+  document.getElementById("beer-count").innerText = itemCounts.beer;
+
   document.getElementById("count").innerText = itemsOrdered;
   document.getElementById("price").innerText = totalPrice;
 }
 
-function askTea() {
-  teaCount++;
+function orderItem(item) {
+  itemCounts[item]++
   itemsOrdered++;
-  totalPrice += 280;
-  document.getElementById("tea-count").innerText = teaCount;
-  document.getElementById("count").innerText = itemsOrdered;
-  document.getElementById("price").innerText = totalPrice;
-}
-
-function askMilk() {
-  milkCount++;
-  itemsOrdered++;
-  totalPrice += 180;
-  document.getElementById("milk-count").innerText = milkCount;
-  document.getElementById("count").innerText = itemsOrdered;
-  document.getElementById("price").innerText = totalPrice;
-}
-
-function askCoke() {
-  cokeCount++;
-  itemsOrdered++;
-  totalPrice += 190;
-  document.getElementById("coke-count").innerText = cokeCount;
-  document.getElementById("count").innerText = itemsOrdered;
-  document.getElementById("price").innerText = totalPrice;
-}
-
-function askBeer() {
-  beerCount++;
-  itemsOrdered++;
-  totalPrice += 580;
-  document.getElementById("beer-count").innerText = beerCount;
-  document.getElementById("count").innerText = itemsOrdered;
-  document.getElementById("price").innerText = totalPrice;
+  totalPrice += prices[item];
+  updateUI();
 }
